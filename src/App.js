1e379b459a67fac08537b782d9aac9a19a2fac32
Tianzi Cui <cuitianzi0@gmail.com>
2024-06-12 21:52:37 -0600

    toggle and delete finished

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { useState } from "react";
+
+const shoppingList = [
+  { id: 0, quantity: 2, name: "salmon", bought: false },
+  { id: 1, quantity: 1, name: "toast", bought: false },
+  { id: 3, quantity: 18, name: "eggs", bought: false },
+];
+
 export default function App() {
   return (
     <div className="App">
@@ -7,12 +15,34 @@ export default function App() {
 }
 
 function GroceryList() {
+  const [newShoppingList, setNewShoppingList] = useState(shoppingList);
+
+  function handleNewItem(newItem) {
+    setNewShoppingList((list) => [...newShoppingList, newItem]);
+  }
+
+  function handleToggleItem(id) {
+    setNewShoppingList((arr) =>
+      arr.map((item) =>
+        item.id === id ? { ...item, bought: !item.bought } : { ...item }
+      )
+    );
+  }
+
+  function handleDeleteItem(id) {
+    setNewShoppingList((arr) => arr.filter((item) => item.id !== id));
+  }
+
   return (
     <div>
       <Header />
-      <FormAddItems />
-      <ItemList />
-      <Footer/>
+      <FormAddItems onNewItem={handleNewItem} />
+      <ItemList
+        shoppingList={newShoppingList}
+        onToggleItem={handleToggleItem}
+        onDeleteItem={handleDeleteItem}
+      />
+      <Footer />
     </div>
   );
 }
@@ -24,40 +54,67 @@ function Header() {
     </header>
   );
 }
-function FormAddItems() {
+function FormAddItems({ onNewItem }) {
+  const [quantity, setQuantity] = useState(0);
+  const [name, setName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !quantity) return;
+    const id = crypto.randomUUID();
+    const newItem = { id, quantity, name, bought: false };
+    onNewItem(newItem);
+    setQuantity(0);
+    setName("");
+  }
   return (
-    <form className="add-item">
+    <form className="add-item" onSubmit={handleSubmit}>
       <label> 🍟Item Name </label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label> 🕹️Item Quantity</label>
-      <input type="number" />
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
 
-function ItemList() {
+function ItemList({ shoppingList, onToggleItem, onDeleteItem }) {
   return (
     <div className="item-list">
-      <ul >
-        <Item />
+      <ul>
+        {shoppingList.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            onToggleItem={onToggleItem}
+            onDeleteItem={onDeleteItem}
+          />
+        ))}
       </ul>
     </div>
   );
 }
 
-function Item() {
+function Item({ item, onToggleItem, onDeleteItem }) {
   return (
     <li>
-      <input type="checkbox" />
-      Salmon
-      <span>❌</span>
+      <input type="checkbox" onClick={() => onToggleItem(item.id)} />
+      <span style={item.bought ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Footer(){
-  return <footer className="footer">
-    Copy right reserved for Lexie 2024
-  </footer>
+function Footer() {
+  return <footer className="footer">&copy; 2024 Lexie. All rights reserved.</footer>;
 }
